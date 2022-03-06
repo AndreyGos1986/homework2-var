7d2eb8975554f4522b1bd76f6f0e293d633770af
Андрей <gosudarev_a@mail.ru>
2022-03-06 09:33:57 +0300

    Домашка по переменным с дополнением задач повышенной сложности №6, 7 и 8 (корректировки в решении 7 задачи)

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -141,8 +141,9 @@ public class Main {
 
         short a2 = 7;
         short b2 = 5;
-        System.out.println("a2 = " + (b2+b2+b2)/3);
-        System.out.println("b2 = " + (a2+a2+a2)/3);
+
+        System.out.println("a2 = " + (a2*b2)/a2);
+        System.out.println("b2 = " + (a2*b2)/b2);
 
         System.out.println();
         System.out.println(" ЗАДАЧА №8 ");
