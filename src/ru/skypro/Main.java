a37c11ca18f183ccd72f8093b01acdca78900d63
Андрей <gosudarev_a@mail.ru>
2022-03-07 08:04:03 +0300

    Корректировка 7 задачи

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -141,11 +141,14 @@ public class Main {
 
         int a2 = 7;
         int b2 = 5;
-        a2=(a2*b2)/a2;
-        if (a2==b2) b2=7;
 
-        System.out.println("a2 = " + a2);
+        a2=a2*b2;
+
+        b2=a2/b2;
         System.out.println("b2 = " + b2);
+        a2=a2/b2;
+        System.out.println("a2 = " + a2);
+
 
         System.out.println();
         System.out.println(" ЗАДАЧА №8 ");
